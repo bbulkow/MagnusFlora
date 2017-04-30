e038b1125f74d128ccb63a814af96876c9ac92c9
bbulkow <brian@bulkowski.org>
2017-04-30 00:02:56 +0000

    Actually plays sounds!!!

--- samples/sound.aplay.py
+++ samples/sound.aplay.py
@@ -1,9 +1,15 @@
 import sys
+import os
+import time
 
-import sounddevice as sd
-import soundfile as sff
-
+# this helps me know how long in time a file will play
 import wave
+
+import random
+
+import subprocess
+
+
 
 
 """
@@ -35,38 +41,84 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SO
 
 """
 
+# why are these lists? Because it's cool to have a selection of different sounds
+# this will randomly play one from the list
 
-#reso_destroyed = "../audio/resonator_destroyed1.wav"
-#reso_deployed = "../audio/resonator_deployed1.wav"
-#test_file = "../audio/violin-test-PCM16-48.wav"
-test_file = "../audio/violin-test-PCM16.wav"
+game_audio_files = {
+	'hack_neutral': [ '../audio/neutral_portal_hack.wav' ],
+	'hack_res': [ '../audio/portal_hack_res.wav' ],
+	'hack_enl': [ ],
+	'portal_neutralized': [ '../audio/portal_neutralized.wav' ],
+	'portal_online': [ '../audio/portal_online.wav' ],
+	'portal_deployed': [ '../audio/resonator_deployed_1.wav' ],
+	'reso_destroyed': [ '../audio/resonator_destroyed_1.wav', '../audio/resonator_destroyed_2.wav'],
+	'under_attack': [ '../audio/under_attack.wav' ],
+}
+
+background_sounds = [
+	'../audio/violin-test-PCM16.wav' ]
+
+# insert the filename at the command: so 1 is right after the command
+#command_filename_offset = 3
+#command_template = [ 'aplay', ' -f','cd' ]
+command_filename_offset = 1
+command_template = [ 'aplay' ]
 
 
-CHUNK = 1024 * 16
+# returns some kind of object to allow killing
+def play_sound( filename ):
+	global command_template
+	global command_filename_offset
 
-print("wave open")
+	stat = os.stat( filename )
+	print("f_bytes: ",stat.st_size)
 
-wf = wave.open(test_file, 'rb')
-print (" wave object: channels ",wf.getnchannels()," rate ",wf.getframerate()," samp width ",wf.getsampwidth() )
+	# let's check the length
+	wf = wave.open(filename, 'rb')
+	print (" wave object: channels ",wf.getnchannels()," rate ",wf.getframerate()," samp width ",wf.getsampwidth() )
+	bytes_per_second = wf.getnchannels() * wf.getframerate() * wf.getsampwidth()
+	print (" bytes per second: ",bytes_per_second)
+	sec = stat.st_size / bytes_per_second
+	print ("seconds is: ",sec)
 
-duration = 5
-device = 0
-dtype = 'int16'
+	ct = list(command_template)
+	ct.insert(command_filename_offset, filename)
+	print(" passing to popen: ", ct)
+	proc = subprocess.Popen( ct )
 
-with sff.SoundFile(test_file) as sf:
-    def callback(outdata, frame_count, time, status):
-        data = sf.read(frame_count, dtype=dtype, out=outdata)
-        if (data.size != outdata.size): 
-            # zero the part of outdata not written into by sf.read()
-            outdata[data.shape[0]:,:].fill(0.0)
-            raise sd.CallbackStop()
+	print (" delaying ") 
+	time.sleep( sec - 1.0 )
 
-    with sd.OutputStream(device=device, samplerate=sf.samplerate,
-                        channels=sf.channels,
-                        dtype=dtype,
-                        callback=callback) as ss:
-        while (ss.active):
-            sd.sleep(duration * 1000)
+	# test: kill the sound
+	print (" killing " )
+	proc.kill()
+	
+
+
+play_sound( '../audio/portal_hack_res.wav' )
+
+def loop_sounds():
+
+
+	for key in game_audio_files:
+		fl = game_audio_files[ key ]
+		if len(fl) == 0:
+			continue
+		elif len(fl) == 1:
+			fn = fl[0]
+		else:
+			fn = fl[ random.randint(0,len(fl)-1) ]
+		print (" playing audio file: ",fn )
+
+		# let's check the length
+		wf = wave.open(fn, 'rb')
+		print (" wave object: channels ",wf.getnchannels()," rate ",wf.getframerate()," samp width ",wf.getsampwidth() )
+
+		play_sound(fn)
+
+loop_sounds()
+
+
 
 
 
