a705e21c3aeba9370c0faf57f3c1ed2c19930bfe
bbulkow <brian@bulkowski.org>
2017-10-07 20:05:36 +0000

    Add the scripts for setting volume and such

---
+++ audio/LOOPSONG.sh
@@ -0,0 +1,5 @@
+cd ~/MagnusFlora/audio
+while [ 1 ]
+do
+    aplay magnus_the_song.wav
+done
---
+++ audio/SOUNDLOOP.sh
@@ -0,0 +1,7 @@
+cd ~/MagnusFlora/audio
+while [ 1 ]
+do
+    aplay magnus_the_song.wav
+    aplay background_ENL.wav
+    aplay background_RES.wav
+done
